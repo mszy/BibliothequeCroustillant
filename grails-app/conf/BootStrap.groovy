0801403af667b7c0582947e1c8c4b52a6c82287c
Mathieu SOUM <mathieu.soum@gmail.com>
2014-04-15 17:22:49 +0200

    Boostrap ne récupère plus que les entrées qui ont auteur et type

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,33 +2,33 @@ import bibliothequecroustillant.Auteur
 import bibliothequecroustillant.Livre
 import bibliothequecroustillant.TypeDocument
 
+
 class BootStrap {
 
     def init = { servletContext ->
 		def csv = new File("books.csv")
 		csv.splitEachLine(';') { row ->
-			def auteur
-			if( !row[4] ) {
-				auteur = null
-			} else {
+			if( row[4] != null && !row[1].isEmpty() ) {
 				def rawAuteur = row[4].tokenize(", ");
 				def prenomAuteur = rawAuteur[1]
 				def nomAuteur = rawAuteur[0]
-				auteur = Auteur.findByNom(nomAuteur) ?: new Auteur( nom: nomAuteur, prenom: prenomAuteur )
+				def auteur = Auteur.findByNom(nomAuteur) ?: new Auteur( nom: nomAuteur, prenom: prenomAuteur )
 				auteur.save()
+				
+				def typeDocument = TypeDocument.findByIntitule(row[1]) ?: new TypeDocument( intitule: row[1] )
+				typeDocument.save()
+				
+			    def livre = new Livre(
+					typeDocument: typeDocument,
+				    titre: row[3],
+				    qteDispo: 10,
+				    qteTotale: 10
+				)
+				
+				if ( auteur != null) {
+					livre.addToAuteurs(auteur)
+				}
 			}
-			def typeDocument = TypeDocument.findByIntitule(row[1]) ?: new TypeDocument( intitule: row[1] )
-			typeDocument.save()
-		    def livre = new Livre(
-			    typeDocument: typeDocument,
-			    titre: row[3],
-			    nombreExemplairesDispo: 10,
-			    nombreExemplairesTotal: 10
-			)
-			if ( auteur ) {
-				livre.addToAuteurs(auteur)
-			}
-			livre.save()
 		}
     }
     def destroy = {
